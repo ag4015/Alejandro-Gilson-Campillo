b2f5e2a702e7c0e97d32c781c8effc24e055a09d
ag4015 <alex.gilsoncampillo@gmail.com>
2018-05-15 12:51:29 +0100

    Added javascript to detect when the user is mobile

---
+++ scripts/style.js
@@ -0,0 +1,23 @@
+
+
+
+function isMobileDevice() {
+   if ( navigator.userAgent.match(/Android/i) ||
+        navigator.userAgent.match(/webOS/i) ||
+        navigator.userAgent.match(/iPhone/i) ||
+        navigator.userAgent.match(/iPad/i) ||
+        navigator.userAgent.match(/iPod/i) ||
+        navigator.userAgent.match(/BlackBerry/i) ||
+        navigator.userAgent.match(/Windows Phone/i) ||
+        navigator.userAgent.match(/IEMobile/i) ||
+        navigator.userAgent.match(/Opera Mini/i)
+        )
+ {
+       document.write('<link rel="stylesheet" type="text/css" href="styles/mobile.css">');
+ }
+ else {
+       document.write('<link rel="stylesheet" type="text/css" href="styles/fixed.css">');
+ }
+}
+
+isMobileDevice();
