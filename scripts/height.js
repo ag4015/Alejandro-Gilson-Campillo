2b54223918a865ce94a1ede4b60c5f37e2c57be8
ag4015 <alex.gilsoncampillo@gmail.com>
2018-05-15 17:39:32 +0100

    Fixed vertical resizing of window. Added height.js

---
+++ scripts/height.js
@@ -0,0 +1,10 @@
+
+
+//  Get Elements
+var topbar = document.querySelector('#topbar');
+var garden = document.querySelector('.garden')
+// Get Screen height
+height = screen.height;
+
+topbar.style.height = (height/15 + "px");
+//garden.style.height = (height + "px");
