b92eb48538bc92cad20082eb8a7fcd34052edee9
ag4015 <alex.gilsoncampillo@gmail.com>
2018-05-15 18:39:56 +0100

    Finished topbar and title

--- scripts/height.js
+++ scripts/height.js
@@ -2,9 +2,11 @@
 
 //  Get Elements
 var topbar = document.querySelector('#topbar');
+var topbarMobile = document.querySelector('#topbarMobile')
 var garden = document.querySelector('.garden')
 // Get Screen height
 height = screen.height;
 
 topbar.style.height = (height/15 + "px");
+topbarMobile.style.height = (height/15 + "px");
 //garden.style.height = (height + "px");
