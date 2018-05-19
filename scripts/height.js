ade2a7be575fae8a639d37b7b5e14ff7d0bb2e73
ag4015 <alex.gilsoncampillo@gmail.com>
2018-05-19 19:05:18 +0100

    Finished My projects section and made it responsive

--- scripts/height.js
+++ scripts/height.js
@@ -4,9 +4,12 @@
 var topbar = document.querySelector('#topbar');
 var topbarMobile = document.querySelector('#topbarMobile')
 var garden = document.querySelector('.garden')
+var project = document.querySelector('.project')
 // Get Screen height
 height = screen.height;
+width = screen.width;
 
 topbar.style.height = (height/15 + "px");
 topbarMobile.style.height = (height/15 + "px");
+
 //garden.style.height = (height + "px");
---
+++ scripts/projectSize.js
@@ -0,0 +1,25 @@
+
+// Get element
+var project1 = document.querySelector('.project-1');
+var project2 = document.querySelector('.project-2');
+var project3 = document.querySelector('.project-3');
+var project4 = document.querySelector('.project-4');
+var project5 = document.querySelector('.project-5');
+var project6 = document.querySelector('.project-6');
+
+// Get screen width
+width = screen.width;
+
+// Set project div width and height
+project1.style.width = (width/5 + "px");
+project1.style.height = (width/8 + "px");
+project2.style.width = (width/5 + "px");
+project2.style.height = (width/8 + "px");
+project3.style.width = (width/5 + "px");
+project3.style.height = (width/8 + "px");
+project4.style.width = (width/5 + "px");
+project4.style.height = (width/8 + "px");
+project5.style.width = (width/5 + "px");
+project5.style.height = (width/8 + "px");
+project6.style.width = (width/5 + "px");
+project6.style.height = (width/8 + "px");
