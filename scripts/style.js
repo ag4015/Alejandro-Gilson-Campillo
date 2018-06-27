e842849583edc4286496125fb11209ac29d7021c
ag4015 <alex.gilsoncampillo@gmail.com>
2018-06-27 15:10:21 +0100

    increased topbarmobile

--- scripts/style.js
+++ scripts/style.js
@@ -13,15 +13,11 @@ function isMobileDevice() {
         navigator.userAgent.match(/Opera Mini/i)
         )
  {
-       // document.write('<link rel="stylesheet" type="text/css" href="styles/mobile.css">');
-       // var topbarMobile = document.querySelector('topbarMobile');
-       // topbarMobile.style.height = (height/7 + "px");
-       // var everything = document.querySelector('html');
-       // everything.style.fontSize = "30px";
+       document.write('<link rel="stylesheet" type="text/css" href="styles/mobile.css">');
  }
  else {
-       // document.write('<link rel="stylesheet" type="text/css" href="styles/fixed.css">');
+       document.write('<link rel="stylesheet" type="text/css" href="styles/fixed.css">');
  }
 }
-document.write('<link rel="stylesheet" type="text/css" href="styles/fixed.css">');
+// document.write('<link rel="stylesheet" type="text/css" href="styles/fixed.css">');
 //isMobileDevice();
